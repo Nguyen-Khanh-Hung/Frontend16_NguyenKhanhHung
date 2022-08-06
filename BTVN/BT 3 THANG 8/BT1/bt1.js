3f765f502565853eb45aa32f2705d66e47cccac5
Nguyen Khanh Hung <hungnk19406c@st.uel.edu.vn>
2022-08-06 19:34:46 +0700

    hehe

--- BTVN/BT 3 THANG 8/BT1/bt1.js
+++ BTVN/BT 3 THANG 8/BT1/bt1.js
@@ -1,8 +1,8 @@
 var node= document.querySelector('.telecom');
 
+// g để tìm hết tất cả các thằng
 
-
-var result=node.innerText.replace(/Frontend/g,`<span class="text">Frontend</span>`)   
+var result=node.innerText.replace(/Frontend/gi,'<span class="text">Frontend</span>')   
 
 node.innerHTML=result
 
